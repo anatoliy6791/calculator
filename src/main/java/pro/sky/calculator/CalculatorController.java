ea899a5a5d7eab67bf2ab3dacab490154f672fa6
user <anatoliy6791>
2022-06-02 23:31:21 +0300

    HW spring

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -23,22 +23,36 @@ public class CalculatorController {
     }
 
     @GetMapping(path ="/plus")
-    public String calcSum(@RequestParam(name = "num1") int a,  @RequestParam(name = "num2")  int b) {
+    public String calcSum(@RequestParam(name = "num1") Integer a,  @RequestParam(name = "num2")  Integer b) {
+        if (a == null || b == null) {
+            return ("Вы не ввели число");
+        }
         return a + " + " + b + " = " + calculatorServiceIml.calculatorSum(a, b);
     }
 
     @GetMapping(path ="/minus")
-    public String calcMinus(@RequestParam(name = "num1") int a,  @RequestParam(name = "num2")  int b) {
+    public String calcMinus(@RequestParam(name = "num1") Integer a,  @RequestParam(name = "num2")  Integer b) {
+        if (a == null || b == null) {
+            return "Вы не ввели число";
+        }
         return a + " - " + b + " = " + calculatorServiceIml.calculatorMinus(a, b);
     }
 
     @GetMapping(path ="/multiply")
-    public String calcMultipication(@RequestParam(name = "num1") int a,  @RequestParam(name = "num2")  int b) {
+    public String calcMultipication(@RequestParam(name = "num1") Integer a,  @RequestParam(name = "num2")  Integer b) {
+        if (a == null || b == null) {
+            return "Вы не ввели число";
+        }
         return a + " * " + b + " = " + calculatorServiceIml.calculatorMultiplication(a,b);
     }
 
     @GetMapping(path ="/divide")
-    public String calcDivision(@RequestParam(name = "num1") BigDecimal a, @RequestParam(name = "num2")  BigDecimal b) {
+    public String calcDivision(@RequestParam(name = "num1") BigDecimal a, @RequestParam(name = "num2")  BigDecimal b) throws RuntimeException {
+        if (a == null || b == null) {
+            return ("Вы не ввели число");
+        } else if (b.equals(new BigDecimal(0))) {
+            return "На ноль делить нельзя";
+        }
         return a + " / " + b + " = " + calculatorServiceIml.calculatorDivision(a, b);
     }
 }
--- src/main/java/pro/sky/calculator/CalculatorServiceIml.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceIml.java
@@ -14,43 +14,29 @@ public class CalculatorServiceIml implements CalculatorService {
 
     @Override
     public int calculatorSum(Integer num1, Integer num2) {
-        if (num1 == null || num2 == null) {
-            throw new RuntimeException("Вы не ввели число");
-        } else {
+
             int result = num1 + num2;
             return result;
         }
-    }
+
 
     @Override
     public int calculatorMinus(Integer num1, Integer num2) {
-        if (num1 == null || num2 == null) {
-            throw new RuntimeException ("Вы не ввели число");
-        } else {
             int result = num1 - num2;
             return result;
         }
-    }
+
 
     @Override
     public int calculatorMultiplication(Integer num1, Integer num2) {
-        if (num1 == null || num2 == null) {
-            throw new RuntimeException ("Вы не ввели число");
-        } else {
             int result = num1 * num2;
             return result;
         }
-    }
+
 
     @Override
     public BigDecimal calculatorDivision(BigDecimal num1, BigDecimal num2) {
-        if (num1 == null || num2 == null) {
-            throw new RuntimeException ("Вы не ввели число");
-        } else if (num2.equals(new BigDecimal(0))) {
-            throw new RuntimeException ("На ноль делить нельзя");
-        } else {
             BigDecimal result = num1.divide(num2);
             return result;
         }
     }
-}
